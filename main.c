878d82d1e58e34cfe939986ff2beec3a9d12d643
camilajullyane <camilaj.silva0@gmail.com>
2024-01-30 18:21:41 -0300

    update

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
 #include "raylib.h"
 
+float radius = 20;
+
 int main() {
-    InitWindow(800, 450, "Hello World");
+    int screenWidth = 800;
+    int screenHeight = 500;
+    int vectorX = 50;
+    int vectorY = 100;
+
+
+    InitWindow(screenWidth, screenHeight, "Flappy Bird");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         //PARTE 1: Processar lógica do jogo
-
         //PARTE 2: Desenhar na tela
         BeginDrawing();
-
+            HideCursor();
+            ClearBackground(SKYBLUE);
+            
+            // DrawCircleV(position, radius, DARKGRAY);
+            DrawCircle(vectorX, vectorY, radius, DARKGRAY);
         EndDrawing();
     }
 
