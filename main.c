01aed1157c659f3835a7057c3d31c4a6fe954a85
devictor8 <joaosalgado605@gmail.com>
2024-01-31 14:16:01 -0300

    GALOPEIIIIRAAAAAAAAAAAAAAAAAAA

--- main.c
+++ main.c
@@ -1,28 +1,137 @@
 #include "raylib.h"
+#include "raymath.h"
 
-float radius = 20;
+#define G 1000
+#define PLAYER_JUMP_SPD 350.0f
+#define PLAYER_HOR_SPD 200.0f
+#define BG_SPEED 350.0f
 
-int main() {
-    int screenWidth = 800;
-    int screenHeight = 500;
-    int vectorX = 50;
-    int vectorY = 100;
+typedef struct Player {
+    Vector2 position;
+    float speed;
+    bool canJump;
+} Player;
 
+typedef struct EnvItem {
+    Rectangle rect;
+    int blocking;
+    Color color;
+} EnvItem;
 
-    InitWindow(screenWidth, screenHeight, "Flappy Bird");
+//----------------------------------------------------------------------------------
+// Module functions declaration
+//----------------------------------------------------------------------------------
+void UpdatePlayer(Player *player, EnvItem *envItems, int envItemsLength, float delta);
+//-----------------------------------------------------------------------------------
+// Program main entry point
+//-----------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //-------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Fish Bird");
+
+    Player player = { 0 };
+    player.position = (Vector2){ -150, 150 };
+    player.speed = 0;
+    player.canJump = false;
+    EnvItem envItems[] = {
+        {{ 0, 400, 1000, 200 }, 1, GRAY },
+        {{ 300, 200, 400, 10 }, 1, GRAY },
+        {{ 250, 300, 100, 10 }, 1, GRAY },
+        {{ 650, 300, 100, 10 }, 1, GRAY }
+    };
+
+    int envItemsLength = sizeof(envItems)/sizeof(envItems[0]);
+
+    Camera2D camera = { 12 };
+    camera.target = (Vector2){50, 200};
+    camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
     SetTargetFPS(60);
+    //--------------------------------------------------------------------------------------
 
-    while (!WindowShouldClose()) {
-        //PARTE 1: Processar lógica do jogo
-        //PARTE 2: Desenhar na tela
+    // Main game loop
+    while (!WindowShouldClose())
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        float deltaTime = GetFrameTime();
+
+        UpdatePlayer(&player, envItems, envItemsLength, deltaTime);
+        //-----------------------------------------------------------------------------
+
+        // Draw 
+        //----------------------------------------------------------------------------------
         BeginDrawing();
-            HideCursor();
-            ClearBackground(SKYBLUE);
-            
-            // DrawCircleV(position, radius, DARKGRAY);
-            DrawCircle(vectorX, vectorY, radius, DARKGRAY);
+
+            ClearBackground(BLUE);
+
+            BeginMode2D(camera);
+                float speed = 100.0f;
+                for (int i = 0; i < envItemsLength; i++) {
+                    envItems[i].rect.x -= speed * GetFrameTime(); // Atualiza a posição x do item
+
+                    // Se o item saiu completamente da tela, move-o de volta para a direita
+                    if (envItems[i].rect.x + envItems[i].rect.width < player.position.x) {
+                    envItems[i].rect.x += screenWidth + envItems[i].rect.width;
+                    }
+                    DrawRectangleRec(envItems[i].rect, envItems[i].color);
+                }
+
+                Rectangle playerRect = { player.position.x - 20, player.position.y - 40, 40, 40 };
+                DrawRectangleRec(playerRect, RED);
+                
+                DrawCircle(player.position.x, player.position.y, 5, GOLD);
+
+            EndMode2D();
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    CloseWindow();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
+void UpdatePlayer(Player *player, EnvItem *envItems, int envItemsLength, float delta)
+{
+    if (IsKeyDown(KEY_SPACE))
+    {
+        player->speed = -PLAYER_JUMP_SPD;
+    }
+    // esse trecho de código verifica se atingiu um obstaculo
+    bool hitObstacle = false;
+    for (int i = 0; i < envItemsLength; i++)
+    {
+        EnvItem *ei = envItems + i;
+        Vector2 *p = &(player->position);
+        if (ei->blocking &&
+            ei->rect.x <= p->x &&
+            ei->rect.x + ei->rect.width >= p->x &&
+            ei->rect.y >= p->y &&
+            ei->rect.y <= p->y + player->speed*delta)
+        {
+            hitObstacle = true;
+            player->speed = 0.0f;
+            p->y = ei->rect.y;
+            break;
+        }
+    }
+
+    if (!hitObstacle)
+    {
+        player->position.y += player->speed*delta;
+        player->speed += G*delta;
+        player->canJump = false;
+    }
+    else player->canJump = true;
 }
