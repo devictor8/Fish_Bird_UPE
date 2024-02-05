5d5a16d56da127b695fdb3a16dd23c48db016836
devictor8 <joaosalgado605@gmail.com>
2024-02-05 19:20:57 -0300

    .

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #define PLAYER_HOR_SPD 200.0f
 #define BG_SPEED 350.0f
 #define ENV_ITEMS_LENGTH 4
+#define NUM_TEXTURES 4
 
 typedef struct Player {
     Vector2 position;
@@ -23,36 +24,34 @@ typedef struct EnvItem {
     Texture2D texture;
 } EnvItem;
 
-//----------------------------------------------------------------------------------
-// Module functions declaration
-//----------------------------------------------------------------------------------
 int UpdatePlayer(Player *player, float delta);
 void UpdatePipePosition();
 int RandInt();
 void CreatePipe();
-//-----------------------------------------------------------------------------------
-// Program main entry point
-//-----------------------------------------------------------------------------------
+
 EnvItem envItems[ENV_ITEMS_LENGTH][2];
 int main(void)
 {
-    // Initialization
-    //-------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "Fish Bird");
     InitAudioDevice();
-    Texture2D fish = LoadTexture("./images/peixe-padrao-pq.png");
+    Texture2D fishTextures[NUM_TEXTURES];
+    fishTextures[0] = LoadTexture("./images/fish-fin-default.png");
+    fishTextures[1] = LoadTexture("./images/fish-fin-up.png");
+    fishTextures[2] = LoadTexture("./images/fish-fin-down.png");
     Texture2D pipeFloor = LoadTexture("./images/cano-baixo.png");
     Texture2D pipeCeiling = LoadTexture("./images/cano-cima.png");
     Music music = LoadMusicStream("./images/xuxa.mp3");
     Sound music2 = LoadSound("./audio/point.ogg");
     Sound hit = LoadSound("./audio/hit.ogg");
-    SetSoundVolume(hit, 0.2f);
+    SetSoundVolume(hit, 0.3f);
+    SetSoundVolume(music2, 0.3f);
     PlayMusicStream(music);
     SetMusicVolume(music, 0.2f);
 
+    int frameCounter = 0;
     Player player = { 0 };
     player.position = (Vector2){ -150, 150 };
     player.score = 0;
@@ -67,45 +66,53 @@ int main(void)
     camera.zoom = 1.0f;
 
     SetTargetFPS(60);
-    //--------------------------------------------------------------------------------------
 
-    int screen  = 0;
+    int screen  = -1;
     while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        float deltaTime = GetFrameTime();
 
-        screen = UpdatePlayer(&player, deltaTime);
+        float deltaTime = GetFrameTime();
+        
         camera.zoom += ((float)GetMouseWheelMove()*0.05f);
 
         if (camera.zoom > 3.0f) camera.zoom = 3.0f;
         else if (camera.zoom < 0.25f) camera.zoom = 0.25f;
         UpdateMusicStream(music);
-
-        //-----------------------------------------------------------------------------
-
-        // Draw 
-        //----------------------------------------------------------------------------------
-        // DrawTextureV(texture, player.position, WHITE); 
         
         BeginDrawing();
 
-        if (screen == 0) {
+        if (screen == -1) 
+        {
+            ClearBackground(BLUE);
+            DrawTexture(fishTextures[0], player.position.x + 300, player.position.y, WHITE); 
+            DrawText("Press Enter to start", 80, 210, 20, WHITE);
+            for (int i = 0; i < ENV_ITEMS_LENGTH; i++) 
+            {
+                DrawTextureV(envItems[i][0].texture, (Vector2){envItems[i][0].rect.x + 320, envItems[i][0].rect.y}, WHITE);
+                DrawTextureV(envItems[i][1].texture, (Vector2){envItems[i][1].rect.x + 320, envItems[i][1].rect.y}, WHITE);
+                }
+            if (IsKeyPressed(KEY_ENTER)) screen = 0;        
+        } 
 
-            DrawTexture(fish, player.position.x + 300, player.position.y, WHITE); 
+        if (screen == 0) 
+        {
+            screen = UpdatePlayer(&player, deltaTime);
+            frameCounter++;
+            Texture2D currentFishTexture = fishTextures[(frameCounter / 8) % 2];
+            DrawTexture(currentFishTexture, player.position.x + 300, player.position.y, WHITE);
             ClearBackground(BLUE);
 
             BeginMode2D(camera);
                 float speed = 100.0f;
                 for (int i = 0; i < ENV_ITEMS_LENGTH; i++) {
+                     
                     envItems[i][0].rect.x -= speed * GetFrameTime(); // Atualiza a posição x do item
                     envItems[i][1].rect.x -= speed * GetFrameTime(); // Atualiza a posição x do item
 
                     // Se o item saiu completamente da tela, move-o de volta para a direita
                     
                     DrawTextureV(envItems[i][0].texture, (Vector2){envItems[i][0].rect.x - 20, envItems[i][0].rect.y}, WHITE);
-                    DrawTextureV(envItems[i][1].texture, (Vector2){envItems[i][1]. rect.x - 20, envItems[i][1].rect.y}, WHITE);
+                    DrawTextureV(envItems[i][1].texture, (Vector2){envItems[i][1].rect.x - 20, envItems[i][1].rect.y}, WHITE);
                 }
                 if (envItems[0][0].rect.x + envItems[0][0].rect.width < player.position.x - 150) {
                         UpdatePipePosition();
@@ -117,14 +124,15 @@ int main(void)
                         PlaySound(music2);
                     }
                 }
-        
+
 
             EndMode2D();
             DrawText(TextFormat("Score: %i", player.score), 100, 30, 20, BLACK);
             PlaySound(hit);
         }
 
-        if (screen == 1) {
+        if (screen == 1) 
+        {
             ClearBackground(BLUE);
             DrawText("Game Over", 300, 170, 40, WHITE);
             DrawText(TextFormat("Score: %i", player.score), 360, 225, 20, WHITE);
@@ -133,23 +141,18 @@ int main(void)
                 player.position = (Vector2){ -150, 150 };
                 player.score = 0;
                 CreatePipe(pipeFloor, pipeCeiling);
-                screen = 0;
+                screen = -1;
             }          
         }
-
         EndDrawing();
-        //               ----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadTexture(fish);
+    UnloadTexture(fishTextures[0]);
+    UnloadTexture(fishTextures[1]);
+    UnloadTexture(fishTextures[2]);
     UnloadTexture(pipeCeiling);
     UnloadTexture(pipeFloor);
     UnloadMusicStream(music);
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
 
@@ -158,8 +161,7 @@ int UpdatePlayer(Player *player, float delta)
     if (IsKeyDown(KEY_SPACE))
     {
         player->speed = -PLAYER_JUMP_SPD;
-    }
-    // esse trecho de código verifica se atingiu um obstaculo
+    }  
     int hitObstacle = 0;
     for (int i = 0; i < ENV_ITEMS_LENGTH; i++)
     {
